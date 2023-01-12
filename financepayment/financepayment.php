1cd755376a7b8d79e1074c70e2d78971cc2f114f
Andrew Smith <andrew.smith@divido.com>
2023-01-12 13:05:36 +0000

    chore: get smaller logo

--- financepayment/financepayment.php
+++ financepayment/financepayment.php
@@ -764,7 +764,7 @@ class FinancePayment extends PaymentModule
         $newOption->setCallToActionText($action);
         $newOption->setAction($this->context->link->getModuleLink($this->name, 'payment', array(), true));
         $newOption->setAdditionalInformation($info);
-        $newOption->setLogo('https://lender-branding-prod.s3.eu-west-1.amazonaws.com/ing.png');
+        $newOption->setLogo('https://lender-branding-test.s3.eu-west-1.amazonaws.com/ing-presta.png');
 
         $payment_options = array($newOption);
 
